19272aa129df021bc50e3890efdb7167f0cde2e8
Nikita Choudhary <84392685+nikita1-pixel@users.noreply.github.com>
2025-10-06 01:39:52 +0530

    blogpage

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -5,8 +5,8 @@ import styles from "./Pagination.module.css";
 const Pagination = () => {
     return (
         <div className={styles.container}>
-            <button className={styles.button}></button>
-            <button className={styles.button}></button>
+            <button className={styles.button}>Previous</button>
+            <button className={styles.button}>Next</button>
         </div>
     )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,44 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <div className={styles.container}>
-            Footer
+            <div className={styles.info}>
+                <div className={styles.logo}>
+                    <img src="/logo.png" alt="logo" className={styles.img} width={50} height={50}/>
+                    <h1 className={styles.logoText}>MindScapes</h1>
+                    <p className={styles.desc}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quaerat asperiores iusto deleniti maiores cumque vel voluptas ex, optio sit odio at nostrum natus doloremque in quae corporis iure alias!
+                    </p>
+                    <div className={styles.icons}>
+                        <img src="/facebook.png" alt="facebook" height={18} width={18} />
+                        <img src="/tiktok.png" alt="tiktok" height={18} width={18} />
+                        <img src="/instagram.png" alt="instagram" height={18} width={18} />
+                        <img src="/youtube.png" alt="youtube" height={18} width={18} />
+                    </div>
+                </div>
+            </div>
+             <div className={styles.links}>
+                <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Style</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Coding</Link>
+          <Link href="/">Travel</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Youtube</Link>
+        </div>
+             </div>
         </div>
     )
 }
