bcba3c22465f3b1765e2dda5f0a1f3cdc5e3ac4d
Nikita Choudhary <84392685+nikita1-pixel@users.noreply.github.com>
2025-10-18 11:56:13 +0530

    complete comments page

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -15,7 +15,19 @@ const Comments = () => {
         </div>
         ) : (
         <Link href="/login">Login to write a comment </Link>)}
-
+        <div className={styles.comments}> 
+            <div className={styles.comment}>
+                <div className={styles.user}>
+                    <img src="/p1.jpeg" alt='' width={50} height={50} className={styles.image}/>
+                    <div className={styles.userInfo}>
+                        <span className={styles.username}>USERNAME</span>
+                        <span className={styles.date}>DATE</span>
+                    </div>
+                </div>
+                <p className={styles.desc}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa cupiditate minus tenetur eveniet id aut laboriosam officiis temporibus. Maxime, tempora id. Adipisci assumenda sunt deleniti! Incidunt natus impedit inventore ratione?</p>
+            </div>
+        </div>
        </div>
   )
 }
